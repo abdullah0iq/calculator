bc5a9cfd9e4735e51f8a8278706ff7bfbef3db2a
abdullah0iq <abdullah.zaid539@gmail.com>
2023-10-09 00:10:05 +0300

    i have done most of the main operations there are some bugs but i didn't take any notes i learned a lot about javascript today next time i will make sure to take notes for the bugs and make some kind of a todo list of bugs

--- script.js
+++ script.js
@@ -1,12 +1,100 @@
-function add(a, b) {
-  return a + b;
+let currentNumber = "";
+let fullOperation = "";
+let result;
+let operation = "";
+let reset = false;
+
+const smallScreen = document.getElementById("smallScreen");
+const mainScreen = document.getElementById("mainScreen");
+const buttonsBox = document.getElementById("buttonsBox");
+
+buttonsBox.addEventListener("click", function (event) {
+  if (reset) {
+    resetCalculator();
+  }
+  if (event.target.hasAttribute("data-number")) {
+    currentNumber += event.target.textContent;
+    mainScreen.textContent = currentNumber;
+  } else if (event.target.hasAttribute("data-operation")) {
+    chosenOperation(event.target);
+  } else if (event.target.id == "clear") resetCalculator();
+  else if (event.target.id == "decimal") addDecimal();
+  else if (event.target.id == "delete") backspace();
+  else if (event.target.id == "equal") equal();
+});
+
+function chosenOperation(target) {
+  if (target.textContent == "+") {
+    operation = "+";
+    screensController();
+  } else if (target.textContent == "/") {
+    operation = "/";
+    screensController();
+  } else if (target.textContent == "X") {
+    operation = "X";
+    screensController();
+  } else if (target.textContent == "-") {
+    operation = "-";
+    screensController();
+  }
 }
-function subtract(a, b) {
-  return a - b;
+function screensController() {
+  if (fullOperation.length == 0) {
+    fullOperation += currentNumber + " " + operation;
+    currentNumber = "";
+    smallScreen.textContent = fullOperation;
+    mainScreen.textContent = "0";
+
+    return;
+  }
+  fullOperation += " " + currentNumber;
+  result = calculate();
+  console.log(result);
+  fullOperation =
+    result.toString().indexOf(".") !== -1
+      ? result.toFixed(5) + " " + operation
+      : result + " " + operation;
+  currentNumber = "";
+  mainScreen.textContent = "0";
+  smallScreen.textContent = fullOperation;
 }
-function multiply(a, b) {
-  return a * b;
+
+function equal() {
+  if (operation.length == 0) return;
+  fullOperation += " " + currentNumber;
+  result = calculate();
+  mainScreen.textContent = result.toFixed(5);
+  smallScreen.textContent = fullOperation;
+  reset = true;
 }
-function divide(a, b) {
-  return a / b;
+
+function calculate() {
+  const [first, operation, second] = fullOperation.split(" ");
+  if (operation == "+") {
+    return Number(first) + Number(second);
+  } else if (operation == "-") {
+    return Number(first) - Number(second);
+  } else if (operation == "X") {
+    return Number(first) * Number(second);
+  } else if (operation == "/") {
+    return Number(first) / Number(second);
+  }
+}
+function backspace() {
+  currentNumber = currentNumber.slice(0, -1);
+  mainScreen.textContent = currentNumber.length > 0 ? currentNumber : "0";
+}
+function resetCalculator() {
+  currentNumber = "";
+  fullOperation = "";
+  result = 0;
+  operation = "";
+  reset = false;
+  mainScreen.textContent = "0";
+  smallScreen.textContent = "";
+}
+function addDecimal() {
+  if (currentNumber.includes(".")) return;
+  currentNumber += Number(currentNumber) == 0 ? "0." : ".";
+  mainScreen.textContent = currentNumber;
 }
